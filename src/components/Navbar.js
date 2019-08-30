af19df603149771a6cb21fb3aac84ecf89e3adba
Eddie Gomez <eddieegomez4@gmail.com>
2019-08-29 20:57:29 -0700

    changed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -13,7 +13,7 @@ const Navbar = class extends React.Component {
   }
 
   toggleHamburger = () => {
-    // toggle the active boolean in the state
+    // toggle the active boolean in the state 
     this.setState(
       {
         active: !this.state.active,
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="SoCali Drone" />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,9 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
+              {/* <Link className="navbar-item" to="/services">
+                Services
+              </Link> */}
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
